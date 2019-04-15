a50a1bd2afc59a2d9ebe0d9936991b56366014e5
Alvin Nava <anava8@mail.greenriver.edu>
2019-04-15 16:12:56 -0400

    Create index page

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+// Practice routing
+
+// Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// Require autoload file
+require_once('vendor/autoload.php');
+
+// Create an instance of the Base class
+$f3 = Base::instance();
+
+// Turn on Fat-Free error reporting
+$f3->set('DEBUG', 3);
+
+// Define a default route
+$f3->route('GET /', function() {
+    echo "FOOD!!!";
+    // Display a view
+    //$view = new Template();
+    //echo $view->render('views/home2.html');
+});
+
+// Run Fat-free
+$f3->run();
